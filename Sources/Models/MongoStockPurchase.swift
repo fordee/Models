7d90bf558bf0b159e628d11266526f3ef35a5762
John Forde <fordee@me.com>
2022-08-23 05:57:50 +1200

    Added productId to MongoStockPurchase.

--- Sources/Models/MongoStockPurchase.swift
+++ Sources/Models/MongoStockPurchase.swift
@@ -10,12 +10,14 @@ import SwiftBSON
 
 public struct MongoStockPurchase: Identifiable, Codable, MongoIdentifiable {
   public var _id: BSONObjectID?
+  public var productId: BSONObjectID
   public let quantity: Int
   public let price: Double
   public let purchaseDate: Date
 
-  public init(_id: BSONObjectID? = nil, quantity: Int, price: Double, purchaseDate: Date) {
+  public init(_id: BSONObjectID? = nil, productId: BSONObjectID, quantity: Int, price: Double, purchaseDate: Date) {
     self._id = _id
+    self.productId = productId
     self.quantity = quantity
     self.price = price
     self.purchaseDate = purchaseDate
