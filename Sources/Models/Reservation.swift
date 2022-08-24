4bbc7d325e1c76330508d4e1cc1d62dd5cef39eb
John Forde <fordee@me.com>
2022-08-25 06:58:21 +1200

    Removed Reservation.

--- Sources/Models/Reservation.swift
+++ Sources/Models/Reservation.swift
@@ -5,34 +5,34 @@
 //  Created by John Forde on 21/08/22.
 //
 
-import Foundation
-import SwiftBSON
-
-public struct Reservation: Identifiable, Codable, MongoIdentifiable {
-  public var _id: BSONObjectID?
-  public let startDate: Date
-  public let endDate: Date
-  public let reservationId: String
-  public let iCalDescription: String
-  public let uid: String
-
-  public init(_id: BSONObjectID? = nil, startDate: Date, endDate: Date, reservationId: String, iCalDescription: String, uid: String) {
-    self._id = _id
-    self.startDate = startDate
-    self.endDate = endDate
-    self.reservationId = reservationId
-    self.iCalDescription = iCalDescription
-    self.uid = uid
-  }
-
-  public var id: String {
-    reservationId
-  }
-
-  public var startDateComponents: DateComponents {
-    Calendar.current.dateComponents([.day, .month, .year], from: startDate)
-  }
-  public var endDateComponents: DateComponents {
-    Calendar.current.dateComponents([.day, .month, .year], from: endDate)
-  }
-}
+//import Foundation
+//import SwiftBSON
+//
+//public struct Reservation: Identifiable, Codable, MongoIdentifiable {
+//  public var _id: BSONObjectID?
+//  public let startDate: Date
+//  public let endDate: Date
+//  public let reservationId: String
+//  public let iCalDescription: String
+//  public let uid: String
+//
+//  public init(_id: BSONObjectID? = nil, startDate: Date, endDate: Date, reservationId: String, iCalDescription: String, uid: String) {
+//    self._id = _id
+//    self.startDate = startDate
+//    self.endDate = endDate
+//    self.reservationId = reservationId
+//    self.iCalDescription = iCalDescription
+//    self.uid = uid
+//  }
+//
+//  public var id: String {
+//    reservationId
+//  }
+//
+//  public var startDateComponents: DateComponents {
+//    Calendar.current.dateComponents([.day, .month, .year], from: startDate)
+//  }
+//  public var endDateComponents: DateComponents {
+//    Calendar.current.dateComponents([.day, .month, .year], from: endDate)
+//  }
+//}
