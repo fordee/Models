281c30063199134018041059040f099ecec6c05b
John Forde <fordee@me.com>
2022-08-28 07:53:23 +1200

    Added new Reservation model based on new extract.

--- Sources/Models/Reservation.swift
+++ Sources/Models/Reservation.swift
@@ -5,34 +5,44 @@
 //  Created by John Forde on 21/08/22.
 //
 
-//import Foundation
-//import SwiftBSON
-//
-//public struct Reservation: Identifiable, Codable, MongoIdentifiable {
-//  public var _id: BSONObjectID?
-//  public let startDate: Date
-//  public let endDate: Date
-//  public let reservationId: String
-//  public let iCalDescription: String
-//  public let uid: String
-//
-//  public init(_id: BSONObjectID? = nil, startDate: Date, endDate: Date, reservationId: String, iCalDescription: String, uid: String) {
-//    self._id = _id
-//    self.startDate = startDate
-//    self.endDate = endDate
-//    self.reservationId = reservationId
-//    self.iCalDescription = iCalDescription
-//    self.uid = uid
-//  }
-//
-//  public var id: String {
-//    reservationId
-//  }
-//
-//  public var startDateComponents: DateComponents {
-//    Calendar.current.dateComponents([.day, .month, .year], from: startDate)
-//  }
-//  public var endDateComponents: DateComponents {
-//    Calendar.current.dateComponents([.day, .month, .year], from: endDate)
-//  }
-//}
+import Foundation
+import SwiftBSON
+
+public struct Reservation: Identifiable, Codable, MongoIdentifiable {
+  public var id: String {
+    reservationId
+  }
+
+  public var _id: BSONObjectID?
+
+  public let reservationId: String
+  public let status: String
+  public let gestName: String
+  public let contact: String
+  public let adultsCount: Int
+  public let childrenCount: Int
+  public let infantCount: Int
+  public let startDate: Date
+  public let endDate: Date
+  public let dayCount: Int
+  public let bookingDate: Date
+  public let listingDescription: String
+  public let earnings: Double
+
+  init(_id: BSONObjectID? = nil, reservationId: String, status: String, gestName: String, contact: String, adultsCount: Int, childrenCount: Int, infantCount: Int, startDate: Date, endDate: Date, dayCount: Int, bookingDate: Date, listingDescription: String, earnings: Double) {
+    self._id = _id
+    self.reservationId = reservationId
+    self.status = status
+    self.gestName = gestName
+    self.contact = contact
+    self.adultsCount = adultsCount
+    self.childrenCount = childrenCount
+    self.infantCount = infantCount
+    self.startDate = startDate
+    self.endDate = endDate
+    self.dayCount = dayCount
+    self.bookingDate = bookingDate
+    self.listingDescription = listingDescription
+    self.earnings = earnings
+  }
+}
